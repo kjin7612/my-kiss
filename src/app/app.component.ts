dcac9bca729adb8a1e395f861a5255740c619dd6
Hyeonjin <kjin7612@gmail.com>
2019-12-17 01:07:20 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kiss-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-kiss';
+}
